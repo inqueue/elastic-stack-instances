2618de6fcec2aa2b16f38f47a015bae1e180474e
Jason Bryan <jbryan@elastic.co>
2019-01-16 18:23:43 -0500

    add configuration

--- instance-init-debian.sh
+++ instance-init-debian.sh
@@ -1,3 +1,13 @@
+# Required environment variables
+# elasticsearch : true/false/unset
+# discovery_nodes: ["ad.dr.e.ss:9300"]
+# cluster_name : some_cluster_name
+# ca_node : true/false/unset
+# CERT_PASSWORD : Elasticsearch certificate password
+# ES_BOOTSTRAP_PW : Elasticsearch bootstrap password
+# kibana : true/false/unset
+# metricbeat : true/false/unset
+
 sudo apt-get update && apt-get upgrade -y
 sudo apt-get install software-properties-common -y
 
@@ -14,10 +24,78 @@ sudo apt-get update
 
 # Install specific package version
 # Elasticsearch
-sudo apt-get install elasticsearch=6.5.4 -y
+if [ "$elasticsearch" ]; then
+    sudo apt-get install elasticsearch=6.5.4 -y
+  
+  # Elasticsearch heap
+    sudo sed -i.bak 's/-Xmx1g/-Xmx256m/; s/-Xms1g/-Xms256m/' /etc/elasticsearch/jvm.options
+
+  # Systemd
+    sudo /bin/systemctl daemon-reload
+    sudo /bin/systemctl enable elasticsearch.service
+
+    if [ "$ca_node" ]; then
+        # Create CA
+        sudo /usr/share/elasticsearch/bin/elasticsearch-certutil ca \
+        --ca-dn "CN=Cluster Support Services" \
+        --days 3650 \
+        --out /etc/elasticsearch/elastic-stack-ca.p12 \
+        --pass "$CERT_PASSWORD"
+
+        sudo /usr/share/elasticsearch/bin/elasticsearch-certutil cert \
+            --ca /etc/elasticsearch/elastic-stack-ca.p12 \
+            --ca-pass "$CERT_PASSWORD" \
+            --days 3650 \
+            --out /etc/elasticsearch/elastic-stack-certificate.p12 \
+            --pass "$CERT_PASSWORD"
+    fi
+
+    sudo chmod g+r /etc/elasticsearch/elastic-stack-ca.p12 /etc/elasticsearch/elastic-stack-certificate.p12
+
+    echo "$ES_BOOTSTRAP_PW" | sudo /usr/share/elasticsearch/bin/elasticsearch-keystore \
+        add -x "bootstrap.password"
+    echo "$CERT_PASSWORD" | sudo /usr/share/elasticsearch/bin/elasticsearch-keystore \
+        add -x xpack.security.transport.ssl.truststore.secure_password
+    echo "$CERT_PASSWORD" | sudo /usr/share/elasticsearch/bin/elasticsearch-keystore \
+        add -x xpack.security.transport.ssl.keystore.secure_password
+    echo "$CERT_PASSWORD" | sudo /usr/share/elasticsearch/bin/elasticsearch-keystore \
+        add -x xpack.security.http.ssl.keystore.secure_password
+    echo "$CERT_PASSWORD" | sudo /usr/share/elasticsearch/bin/elasticsearch-keystore \
+        add -x xpack.security.http.ssl.truststore.secure_password
+    # elasticsearch.yml
+    sudo mv /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.orig
+    cat << EOF | sudo tee /etc/elasticsearch/elasticsearch.yml
+#xpack.license.self_generated.type: trial
+cluster.name: "$cluster_name"
+network.host: _site_,_local_
+node.master: true
+node.data: false
+node.ingest: true
+path.data: /var/lib/elasticsearch
+path.logs: /var/log/elasticsearch
+discovery.zen.ping.unicast.hosts: "$discovery_nodes"
+xpack.security.enabled: true
+xpack.security.transport.ssl.enabled: true
+xpack.security.transport.ssl.verification_mode: certificate 
+xpack.security.transport.ssl.keystore.path: elastic-stack-certificate.p12
+xpack.security.transport.ssl.truststore.path: elastic-stack-ca.p12
+xpack.security.http.ssl.enabled: true
+xpack.security.http.ssl.keystore.path: elastic-stack-certificate.p12
+xpack.security.http.ssl.truststore.path: elastic-stack-ca.p12
+EOF
+fi
+
+# Kibana
+if [ "$kibana" ]; then
+sudo apt-get install kibana=6.5.4
+fi
 
 # Metricbeat
-sudo apt-get install metricbeat=6.5.4 -y
+if [ "$metricbeat" ]; then 
+  sudo apt-get install metricbeat=6.5.4 -y
+fi
 
-# Elasticsearch heap
-sudo sed -i.bak 's/-Xmx1g/-Xmx256m/; s/-Xms1g/-Xms256m/' /etc/elasticsearch/jvm.options
+# kibana.yml
+cat << EOF
+
+EOF
